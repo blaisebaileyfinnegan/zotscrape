c7cd3da14f8273a9243c79162dde3b2c6823b281
Michael Vu <mvu1@uci.edu>
2013-09-18 17:23:46 -0700

    Fixed init

--- migrations/001-init.js
+++ migrations/001-init.js
@@ -1,4 +1,9 @@
 var connection = require('./../lib/db');
+var fs = require('fs');
+
+var options = {
+    encoding: 'utf8'
+}
 
 exports.up = function(next) {
     fs.readFile(__dirname + '/001-init-up.sql', options, function (err, queries) {
